6df016f2414f01cd9fe54e291b73fcdf09aac811
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-06-03 12:43:22 -0400

    User development seeds

---
+++ db/seeds/01-user.js
@@ -0,0 +1,20 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("user")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("user").insert([
+        {
+          user_id: 1,
+          firebase_id: 1,
+          username: "sarah",
+          email: "sarah@email.com",
+          name: "Sarah",
+          latitude: 0,
+          longitude: 0,
+          lend_radius: 1
+        }
+      ]);
+    });
+};
