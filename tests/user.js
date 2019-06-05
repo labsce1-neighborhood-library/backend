80c5c348f61333cf92a166c1d4f0ec991539b521
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-06-05 15:50:35 -0400

    Tests for user table

---
+++ tests/user.js
@@ -0,0 +1,46 @@
+// Import the dependencies for testing
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../index.js");
+const db = require("../db/config.js");
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+
+beforeEach(async () => {
+  await db("user_table").truncate();
+});
+
+describe("User", () => {
+  describe("GET /user/all with empty database", () => {
+    // Test to get all users with empty database
+    it("should return an empty array if database is empty", done => {
+      chai
+        .request(server)
+        .get("/user/all")
+        .end((err, res) => {
+          res.body.should.be.a("array");
+          res.body.should.have.lengthOf(0);
+          done();
+        });
+    });
+  });
+
+  describe("GET /user/all with nonempty database", () => {
+    // Test to get all users
+    it("should return an array of length 1", async () => {
+      await db.seed.run();
+      return new Promise(resolve => {
+        chai
+          .request(server)
+          .get("/user/all")
+          .end((err, res) => {
+            res.body.should.be.a("array");
+            res.body.should.have.lengthOf(1);
+            resolve();
+          });
+      });
+    });
+  });
+});
