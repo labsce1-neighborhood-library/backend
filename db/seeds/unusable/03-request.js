47c4a41e8ee9a5108f78810aa0e0a28637143d09
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-06-06 15:35:41 -0400

    Move unusable seed files

---
+++ db/seeds/unusable/03-request.js
@@ -0,0 +1,14 @@
+// UNUSABLE IF AUTO GENERATING USER SEEDS
+
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex("request_table")
+//     .del()
+//     .then(function() {
+//       // Inserts seed entries
+//       return knex("request_table").insert([
+//         { loaner: 1, borrower: 2, book: 1 },
+//         { loaner: 2, borrower: 3, book: 2 }
+//       ]);
+//     });
+// };
