ab894d3ed8cbc4714199622c27c7ba6c44677f98
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-06-06 15:12:11 -0400

    Organize server middleware file

--- config/middleware.js
+++ config/middleware.js
@@ -2,18 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const testRouter = require("../routers/testRouter.js");
-const userRouter = require("../routers/userRouter.js");
-const bookRouter = require("../routers/bookRouter.js");
+const { userRouter, bookRouter } = require("../routers/index.js");
+
+// const testRouter = require("../routers/testRouter.js");
+// const userRouter = require("../routers/userRouter.js");
+// const bookRouter = require("../routers/bookRouter.js");
 
 module.exports = server => {
   // middleware
   server.use(express.json());
   server.use(cors());
   server.use(helmet());
+  server.use(morgan("dev"));
 
   // express routers
-  server.use("/test", testRouter);
+  // server.use("/test", testRouter);
   server.use("/user", userRouter);
   server.use("/book", bookRouter);
 };
