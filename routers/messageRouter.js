4e840a2cc06d98e05e0ab02b36c438b78d43796e
Kevin Nguyen <kevinn858@gmail.com>
2019-06-20 22:30:45 -0700

    made a checkUser promise and used it in send message endpoint

--- routers/messageRouter.js
+++ routers/messageRouter.js
@@ -13,38 +13,67 @@ router.get("/all", (req, res) => {
         });
 });
 
+// promise that checks if sender and receiver exist in user_table
+function checkUsers(sender_id, receiver_id) {
+    return new Promise(function(resolve, reject) {
+        db("user_table")
+            .where({user_id:sender_id})
+            .then(sender => {
+                if(sender.length === 1){
+                    db("user_table")
+                    .where({user_id:receiver_id})
+                    .then(receiv => {
+                            if(receiv.length === 1){
+                                resolve(1);
+                            }else{
+                                reject(3);
+                            }
+                        }).catch(err => {
+                            reject(4);
+                        });
+                }else{
+                    reject(2);
+                }
+            }).catch(err => {
+                reject(4);
+            });
+      });
+};
+
 // CREATE message
 router.post("/send", (req, res) => {
     const body = req.body;
     const {sender_id, receiver_id} = body;
-    db("user_table")
-        .where({user_id:sender_id})
-        .then(sender => {
-            if(sender.length === 1){
-                db("user_table")
-                    .where({user_id:receiver_id})
-                    .then(receiv => {
-                        if(receiv.length === 1){
-                            db("messages_table")
-                                .insert(body)
-                                .then(response => {
-                                    const response_message = "message sent";
-                                    if(response.rowCount === 0){
-                                        response_message = "send failed";
-                                    }
-                                    res.status(200).json({
-                                        message:response_message,
-                                        response
-                                    });
-                                }).catch(err => res.status(500).json(err.message));
-                        }else{
-                            res.status(404).json({message:"receiver not found", receiver_id});
-                        }
-                    }).catch(err => res.status(500).json(err.message));
-            }else{
+    checkUsers(sender_id, receiver_id)
+        .then(id => {
+            db("messages_table")
+                .insert(body)
+                .then(response => {
+                    const response_message = "message sent";
+                    if(response.rowCount === 0){
+                        response_message = "send failed";
+                    }
+                    res.status(200).json({
+                        message:response_message,
+                        response
+                    });
+                }).catch(err => res.status(500).json(err.message));
+        })
+        .catch(id => {
+            if(id === 2){
                 res.status(404).json({message:"sender not found", sender_id});
+            }else if(id === 3){
+                res.status(404).json({message:"receiver not found", receiver_id});
+            }else{
+                res.status(500).json({message:"server error", id});
             }
-        }).catch(err => res.status(500).json(err.message));
-})
+        });
+});
+
+// READ messages, will truncate to latest 100
+router.get("/get_messages", (req, res) => {
+    const body = req.body;
+    const {sender_id, receiver_id} = body;
+});
 
 module.exports = router;
