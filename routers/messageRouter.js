15db7a9c1922746036ab5e5792c5d22557e2e65b
Kevin Nguyen <kevinn858@gmail.com>
2019-06-20 21:50:35 -0700

    added CREATE message endpoint

--- routers/messageRouter.js
+++ routers/messageRouter.js
@@ -13,4 +13,38 @@ router.get("/all", (req, res) => {
         });
 });
 
+// CREATE message
+router.post("/send", (req, res) => {
+    const body = req.body;
+    const {sender_id, receiver_id} = body;
+    db("user_table")
+        .where({user_id:sender_id})
+        .then(sender => {
+            if(sender.length === 1){
+                db("user_table")
+                    .where({user_id:receiver_id})
+                    .then(receiv => {
+                        if(receiv.length === 1){
+                            db("messages_table")
+                                .insert(body)
+                                .then(response => {
+                                    const response_message = "message sent";
+                                    if(response.rowCount === 0){
+                                        response_message = "send failed";
+                                    }
+                                    res.status(200).json({
+                                        message:response_message,
+                                        response
+                                    });
+                                }).catch(err => res.status(500).json(err.message));
+                        }else{
+                            res.status(404).json({message:"receiver not found", receiver_id});
+                        }
+                    }).catch(err => res.status(500).json(err.message));
+            }else{
+                res.status(404).json({message:"sender not found", sender_id});
+            }
+        }).catch(err => res.status(500).json(err.message));
+})
+
 module.exports = router;
